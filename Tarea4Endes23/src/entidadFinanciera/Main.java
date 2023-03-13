104a57186062d64766859c8ace2006bb73408a33
Luigitime! <antoniors2003@gmail.com>
2023-03-13 20:32:09 +0100

    Nuevos cambios de refactorización añadidos

---
+++ Tarea4Endes23/src/entidadFinanciera/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidadFinanciera;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta objetoCuenta;
+        double saldoActual;
+        
+
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+    
+    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+        c.retirar(cantidad);
+    }
+}
